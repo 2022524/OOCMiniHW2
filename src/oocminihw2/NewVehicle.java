398f6e4d82a93492cf5adf22de6c6f1c07be1901
Tarquin <2022524@student.cct.ie>
2023-10-15 10:42:42 +0100

    Added new Class
    Class : NewVehicle was added to extend and implement the given classes and interfaces. Flyable Override was added too

---
+++ src/oocminihw2/NewVehicle.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocminihw2;
+
+/**
+ *
+ * @author tarq
+ */
+public class NewVehicle extends Vehicle implements Flyable, Sailable {
+// Implementing any abstract methods from FLyable and Sailable interfaces   
+    
+
+// Implementing the method : Flyable
+    @Override
+    public void changeAltitude(float change) {
+    }
+    
+    @Override
+    public float getAltitude(){
+    }
+
+}
